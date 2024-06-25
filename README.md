ceec10af8508b534f1158361c00e98ae9d678af7
Roma <roma150269@gmail.com>
2024-06-25 18:00:41 +0300

    museum landind

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# Base layout template with Gulp, SCSS and Stylelint
-1. Create a repo using this template
-1. Replace `<your_account>` and `<repo_name>` with your Github username and the new repo name
-    - [DEMO LINK](https://<your_account>.github.io/<repo_name>/)
+[DEMO LINK](https://mukutiuk.github.io/museum-landind/)
+
+[LINK-TO-THE-MOCKUP](https://www.figma.com/design/cRBCqE06cDrY3s4jX7h3iY/%D0%9D%D0%90%D0%9C%D0%A3-(Edit)?node-id=0-1&t=fTyYxaqVVDMgOCQg-0)
+
+The landing page is designed and developed using JavaScript, HTML5, CSS3, SCSS, and SASS technologies, ensuring strict adherence to the design in Figma. The header and footer menus contain links that seamlessly navigate users to corresponding sections of the landing page. Consistent animation speed is maintained across the entire page for a cohesive user experience.
+
+Form placeholders provide clear guidance on data input, and form validation prompts users on the correct format for input, including phone numbers. The social media icons for Facebook and Instagram in the footer are interactive and open the museum's respective social profiles in a new tab upon clicking.
+
+In the gallery and exhibitions sections, images enlarge smoothly on hover, enhancing the visual experience for users. Additionally, a smooth scroll feature is implemented for seamless navigation throughout the entire landing page.
+
+To contribute to this project, fork the repository and clone the forked version with a personalized name. Afterward, run 'npm install' or 'npm i' to install dependencies and 'npm start' to launch the project
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,129 @@
-'use strict';
+"use strict";
+
+const buttonOne = document.querySelector("#radio1");
+const buttonTwo = document.querySelector("#radio2");
+const buttonThree = document.querySelector("#radio3");
+const buttonFour = document.querySelector("#radio4");
+let widthImg = 0;
+let slide = 0;
+
+chooseButton();
+
+window.addEventListener("resize", () => {
+  widthImg = document.querySelector(".gallery__img").offsetWidth;
+});
+
+const gallery = document.querySelector(".gallery__container");
+
+buttonOne.addEventListener("click", () => {
+  slide = 0;
+  gallery.style.transform = "translateX(0px)";
+  chooseButton();
+});
+
+buttonTwo.addEventListener("click", () => {
+  slide = 1;
+  gallery.style.transform = `translateX(-${widthImg + 20}px)`;
+  chooseButton();
+});
+
+buttonThree.addEventListener("click", () => {
+  slide = 2;
+  gallery.style.transform = `translateX(-${widthImg * slide + 20 * slide}px)`;
+  chooseButton();
+});
+
+buttonFour.addEventListener("click", () => {
+  slide = 3;
+
+  gallery.style.transform = `translateX(-${widthImg * slide + 20 * slide}px)`;
+  chooseButton();
+});
+
+window.addEventListener("hashchange", () => {
+  if (window.location.hash === "#nav") {
+    document.body.classList.add("page__body--with-menu");
+  } else {
+    document.body.classList.remove("page__body--with-menu");
+  }
+});
+
+const submit = document.querySelector("#myForm");
+
+submit.addEventListener("click", submitClick, false);
+
+function submitClick(event) {
+  event.preventDefault();
+  submit.reset();
+
+  return false;
+}
+
+let positionX = 0;
+
+let isMove = true;
+
+gallery.addEventListener("touchstart", handleTouchStart);
+gallery.addEventListener("touchmove", handleTouchMove);
+
+function handleTouchStart(even) {
+  const firstToch = even.touches[0];
+
+  isMove = true;
+  positionX = firstToch.clientX;
+}
+
+function handleTouchMove(even) {
+  const touchmove = even.touches[0].clientX;
+  const diff = positionX - touchmove;
+
+  if (diff > 0) {
+    if (Math.abs(diff.toFixed(0) > widthImg / 2 && isMove) && slide < 3) {
+      slide++;
+      gallery.style.transform = `translateX(-${slide * (widthImg + 20)}px)`;
+      positionX = 0;
+      isMove = false;
+      chooseButton();
+    }
+  } else {
+    if (diff < widthImg / 2 && isMove) {
+      slide--;
+
+      gallery.style.transform = `translateX(-${slide * (widthImg + 20)}px)`;
+      isMove = false;
+      chooseButton();
+    }
+  }
+}
+
+function chooseButton() {
+  if (slide === 0) {
+    const isActive = document.querySelector(".is-active");
+
+    if (isActive) {
+      isActive.classList.remove("is-active");
+    }
+    buttonOne.classList.add("is-active");
+  } else if (slide === 1) {
+    const isActive = document.querySelector(".is-active");
+
+    if (isActive) {
+      isActive.classList.remove("is-active");
+    }
+    buttonTwo.classList.add("is-active");
+  } else if (slide === 2) {
+    const isActive = document.querySelector(".is-active");
+
+    if (isActive) {
+      isActive.classList.remove("is-active");
+    }
+    buttonThree.classList.add("is-active");
+  } else if (slide === 3) {
+    const isActive = document.querySelector(".is-active");
+
+    if (isActive) {
+      isActive.classList.remove("is-active");
+    }
+    buttonFour.classList.add("is-active");
+  }
+}
